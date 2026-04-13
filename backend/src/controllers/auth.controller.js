652bb4c2423666548bc79b0fa85a30485b51068c
Ria Javalagi <ria.javalagi01@gmail.com>
2026-04-13 13:16:43 +0530

    Login & Logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -56,3 +56,31 @@ export const signup = async (req, res) => {
         return res.status(500).json({message: "Internal server error"});
     }
 };
+
+export const login = async (req,res) => {
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message: "Invalid email or password"});
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) return res.status(400).json({message: "Invalid email or password"});
+
+        generateToken(user._id,res);
+
+        res.status(200).json({
+            _id : user._id,
+            fullName : user.fullName,
+            email : user.email,
+            profilePic : user.profilePic,
+        }); 
+    }catch(error){
+        console.log("Error during login:", error);
+        return res.status(500).json({message: "Internal server error"});
+    }
+};
+export const logout = (_,res) => {
+    res.cookie("jwt", "",{maxAge: 0});
+    res.status(200).json({message: "Logged out successfully"});
+
+};
